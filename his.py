65d1b04da70cf59063eba83cb52aa39d1d41cd71
RubenC <ruben.cecilia.pelaez@gmail.com>
2017-04-26 12:41:38 +0200

    prueba

--- his.py
+++ his.py
@@ -6,8 +6,12 @@
 import sys
 from PyQt5.QtCore import QDir, Qt
 from spectral import *
-from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QApplication, QFileDialog, QLabel, QMessageBox, QSizePolicy, QScrollArea
+import matplotlib
+from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QApplication, QFileDialog, QLabel, QMessageBox, QSizePolicy, QScrollArea, QDialog, QApplication, QPushButton, QVBoxLayout
 from PyQt5.QtGui import QIcon, QPixmap, QColor, QPalette, QImage
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
 
 
 class Example(QMainWindow):
@@ -33,103 +37,76 @@ class Example(QMainWindow):
         openAction.setStatusTip('Open File')
         openAction.triggered.connect(self.showDialog)  
 
-        #fitwindowAction = QAction('Fit to window', self)
-        #fitwindowAction.setShortcut('Ctrl+F')
-        #fitwindowAction.setEnabled(False)
-        #fitwindowAction.isChecked(True)
-        #fitwindowAction.triggered.connect(self.fitToWindow)
+        saveAction = QAction(QIcon('./icons/save.png'), 'Save', self)
+        saveAction.setShortcut('Ctrl+S')
+        saveAction.setStatusTip('Save')
+        saveAction.triggered.connect(self.save)  
+        saveAction.setEnabled(True)
      
 
-        newAction = QAction(QIcon('new.png'), 'New', self)
+        newAction = QAction(QIcon('./icons/new.png'), 'New', self)
         newAction.setShortcut('Ctrl+N')
         newAction.setStatusTip('New File')
 
-        self.zoominAction = QAction(QIcon('zoomin.png'), 'Zoom in',  self)
+        self.zoominAction = QAction(QIcon('./icons/zoomin.png'), 'Zoom in',  self)
         self.zoominAction.setShortcut('Ctrl++')
         self.zoominAction.setStatusTip('Zoom in')
         self.zoominAction.setEnabled(False)
-        self.zoominAction.triggered.connect(self.zoomIn)
+ 
 
-        self.normAction = QAction(QIcon('norm.png'), 'Original Size',  self)
+        self.normAction = QAction(QIcon('./icons/norm.png'), 'Original Size',  self)
         #self.normAction.setShortcut('Ctrl+N')
         self.normAction.setStatusTip('Original Size')
         self.normAction.setEnabled(False)
-        self.normAction.triggered.connect(self.norm)
 
-        self.zoomoutAction = QAction(QIcon('zoomout.png'), 'Zoom out', self)
+        self.zoomoutAction = QAction(QIcon('./icons/zoomout.png'), 'Zoom out', self)
         #self.zoomoutAction.setShortcut('Ctrl+-')
-        self.zoomoutAction.setStatusTip('Zoom out')
+        self.zoomoutAction.setStatusTip('./icons/Zoom out')
         self.zoomoutAction.setEnabled(False)
-        self.zoomoutAction.triggered.connect(self.zoomOut)
 
-        self.cursorAction = QAction(QIcon('cursor.png'), 'Cursor', self)
+        self.cursorAction = QAction(QIcon('./icons/cursor.png'), 'Cursor', self)
         #self.cursorAction.setShortcut('Ctrl+N')
         self.cursorAction.setStatusTip('Cursor')
         self.cursorAction.setEnabled(False)
 
-        self.gridAction = QAction(QIcon('grid.png'), 'Grid', self)
+        self.gridAction = QAction(QIcon('./icons/grid.png'), 'Grid', self)
         self.gridAction.setShortcut('Ctrl+G')
         self.gridAction.setStatusTip('Grid')
         self.gridAction.setEnabled(False)
 
-        self.polylineAction = QAction(QIcon('polyline.png'), 'Draw Polyline', self)
+        self.polylineAction = QAction(QIcon('./icons/polyline.png'), 'Draw Polyline', self)
         self.polylineAction.setShortcut('Ctrl+D')
         self.polylineAction.setStatusTip('Draw Polyline')
         self.polylineAction.setEnabled(False)
 
-        self.analyzeAction = QAction(QIcon('analyze.png'), 'Analyze', self)
+        self.analyzeAction = QAction(QIcon('./icons/analyze.png'), 'Analyze', self)
         self.analyzeAction.setShortcut('Ctrl+A')
         self.analyzeAction.setStatusTip('Analyze')
         self.analyzeAction.setEnabled(False)
 
-        self.joinAction = QAction(QIcon('join.png'), 'Join', self)
+        self.joinAction = QAction(QIcon('./icons/join.png'), 'Join', self)
         self.joinAction.setShortcut('Ctrl+J')
         self.joinAction.setStatusTip('Join')
         self.joinAction.setEnabled(False)
 
-        self.filterinAction = QAction(QIcon('filter.png'), 'Filters', self)
+        self.filterinAction = QAction(QIcon('./icons/filter.png'), 'Filters', self)
         #filterinAction.setShortcut('Ctrl+F')
         self.filterinAction.setStatusTip('Filters')
         self.filterinAction.setEnabled(False)
 
-        self.filterClearAction = QAction(QIcon('ClearFilters.png'), 'Clear Filters', self)
+        self.filterClearAction = QAction(QIcon('./icons/ClearFilters.png'), 'Clear Filters', self)
         self.filterClearAction.setShortcut('Ctrl+C')
         self.filterClearAction.setStatusTip('Clear Filters')
         self.filterClearAction.setEnabled(False)
 
-        self.chart1Action = QAction(QIcon('chart1.png'), 'Chart 1', self)
-        self.chart1Action.setShortcut('Ctrl+1')
-        self.chart1Action.setStatusTip('Chart 1')
-        self.chart1Action.setEnabled(False)
-
-        self.chart2Action = QAction(QIcon('chart2.png'), 'Chart 2', self)
-        self.chart2Action.setShortcut('Ctrl+2')
-        self.chart2Action.setStatusTip('Chart 2')
-        self.chart2Action.setEnabled(False)
-
-        self.chart3Action = QAction(QIcon('chart3.png'), 'Chart 3', self)
-        self.chart3Action.setShortcut('Ctrl+3')
-        self.chart3Action.setStatusTip('Chart 3')
-        self.chart3Action.setEnabled(False)
-
-        self.combochartAction = QAction(QIcon('ComboChart.png'), 'Combo Chart', self)
-        self.combochartAction.setShortcut('Ctrl+4')
-        self.combochartAction.setStatusTip('Chart 4')
-        self.combochartAction.setEnabled(False)
-
-        self.playAction = QAction(QIcon('play.png'), 'Play', self)
-        self.playAction.setShortcut('Ctrl+ ')
-        self.playAction.setStatusTip('Play')
-        self.playAction.setEnabled(False)
 
         self.aboutAction = QAction('About', self)
         self.aboutAction.setStatusTip('About')
         
 
         self.saveAction = QAction('Save', self)
-        self.saveAction.setShortcut('Ctrl+S')
-        self.saveAction.setEnabled(False)
-
+        
+        
         self.statusBar()
 
         menubar = self.menuBar()
@@ -168,23 +145,12 @@ class Example(QMainWindow):
         helpMenu.addAction('Manual')
         helpMenu.addAction(self.aboutAction)
 
-        self.scaleFactor = 0.0
-        #self.printer = QPrinter()
-        self.imageLabel = QLabel()
-        self.imageLabel.setBackgroundRole(QPalette.Base)
-        self.imageLabel.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
-        self.imageLabel.setScaledContents(True)
-
-        self.scrollArea = QScrollArea()
-        self.scrollArea.setBackgroundRole(QPalette.Dark)
-        self.scrollArea.setWidget(self.imageLabel)
-        self.setCentralWidget(self.scrollArea)
-
-
 #TOOLBAR
         toolbar = self.addToolBar('Open')
+
         toolbar.addAction(newAction)
         toolbar.addAction(openAction)
+        toolbar.addAction(saveAction)
         toolbar1 = self.addToolBar('Tools')
         toolbar1.addAction(self.zoominAction)
         toolbar1.addAction(self.normAction)
@@ -195,12 +161,6 @@ class Example(QMainWindow):
         toolbar1.addAction(self.filterinAction)
         toolbar1.addAction(self.filterClearAction)
         toolbar1.addAction(self.joinAction)
-        toolbar1.addAction(self.analyzeAction)
-        toolbar1.addAction(self.chart1Action)
-        toolbar1.addAction(self.chart2Action)
-        toolbar1.addAction(self.chart3Action)
-        toolbar1.addAction(self.combochartAction)
-        toolbar1.addAction(self.playAction)
 
         #icono 
         toolbar2 = self.addToolBar('Exit')
@@ -225,8 +185,8 @@ class Example(QMainWindow):
         #options |= QFileDialog.DontUseNativeDialog
         #fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
         #fname = QFileDialog.getOpenFileName(self, 'Open file', '/desktop')
-        label = QLabel(self)
-        self.setCentralWidget(label)
+        #label = QLabel(self)
+        #self.setCentralWidget(label)
         #pixmap = QPixmap(fileName)
         #img = pixmap.toImage()
         #label.setPixmap(pixmap)
@@ -237,14 +197,16 @@ class Example(QMainWindow):
         #colors = QColor(c).getRgbF()
         #print (colors)
         fileName, _ = QFileDialog.getOpenFileName(self, "Open File", QDir.currentPath())
-        img = open_image(fileName)
-        
-        
-        view = imshow(img, (29, 19, 9))
+        img = open_image(fileName)  
+        view = imshow(img, (29, 19, 9), title=fileName)
+        view.show()
+        # set the layout
+
         #BsqFile
         #view_cube(img, bands=[29, 19, 9])
+        print (fileName)
         print (img)
-        self.saveAction.setEnabled(True)
+
 
         #view_cube(img, bands=[29, 19, 9])
         #if fileName:
@@ -290,36 +252,11 @@ class Example(QMainWindow):
     #
     #    self.updateActions()
 
-    def getPos(self , event):
-        x = event.pos().x()
-        y = event.pos().y() 
-        print (x, y)
+    #Guardar en RGB
+    def save(self):
+        save_rgb('rgb.jpg', img, [29, 19, 9])
 
-    def zoomIn(self):
-        self.scaleImage(1.2)
 
-    def norm(self):
-        self.imageLabel.adjustSize()
-        self.scaleFactor = 1.0
-
-    def zoomOut(self):
-        self.scaleImage(0.8)
-
-    #def updateActions(self):
-    #    self.zoomInAct.setEnabled(not self.fitToWindowAct.isChecked())
-    #    self.zoomOutAct.setEnabled(not self.fitToWindowAct.isChecked())
-    #    self.normalSizeAct.setEnabled(not self.fitToWindowAct.isChecked())
-
-    def scaleImage(self, factor):
-        self.scaleFactor *= factor
-        self.imageLabel.resize(self.scaleFactor * self.imageLabel.pixmap().size())
-
-        self.adjustScrollBar(self.scrollArea.horizontalScrollBar(), factor)
-        self.adjustScrollBar(self.scrollArea.verticalScrollBar(), factor)
-
-    def adjustScrollBar(self, scrollBar, factor):
-        scrollBar.setValue(int(factor * scrollBar.value() + ((factor - 1) * scrollBar.pageStep()/2)))
-   
         
 if __name__ == '__main__':
     
